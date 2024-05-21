1b0edd8d3735b49369e968afd9ae0ec02c170bb4
Shikder Tarek <shikdertarek2019@gmail.com>
2024-05-21 10:16:40 +0600

    Add register page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import Root from "../Root/Root";
 import Home from "../Components/Home/Home";
 import ErrorPage from "../ErrorPage/ErrorPage";
 import Login from "../Components/Login/Login";
+import Register from "../Components/Register/Register";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
       },
       {
         path: "/login",
